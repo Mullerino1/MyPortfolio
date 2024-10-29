9b6cdfc3fee47c9806207d93127b9228ef9b00a3
Mullerino1 <martinemu03@gmail.com>
2024-10-29 19:04:05 +0100

    Update useProjects.tsx

--- react-intro/src/Frontend/hooks/useProjects.tsx
+++ react-intro/src/Frontend/hooks/useProjects.tsx
@@ -65,11 +65,11 @@ export function useProjects(){
 
   const add = async (data: Partial<ProjectType>) => {
     console.log(data + '2')
-    const { title = "", description = "", id= "", createdAt="", updatedAt = "", publishedAt="", status="", visibility=""}  = data
+    const { title = "", description = "", id= "", createdAt="", updatedAt = "", publishedAt="", status="", visibility="", categories=[]}  = data
 
     try {
       setStatus("loading")
-      await projectApi.create({ title, description, id, createdAt, updatedAt, publishedAt, status, visibility})
+      await projectApi.create({ title, description, id, createdAt, updatedAt, publishedAt, status, visibility, categories})
       await fetchData()
       setStatus("success")
     } catch (error) {
