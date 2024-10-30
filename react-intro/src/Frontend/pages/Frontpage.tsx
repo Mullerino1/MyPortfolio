0a9ec7a116d2fd22affaa31304b1dca5af3911a9
Mullerino1 <martinemu03@gmail.com>
2024-10-30 12:50:13 +0100

    stillWorks

--- react-intro/src/Frontend/pages/Frontpage.tsx
+++ react-intro/src/Frontend/pages/Frontpage.tsx
@@ -1,72 +1,71 @@
 
-
-import React from "react";
-import Layout from "../Components/Layout";
+import React, { useEffect, useState } from "react";
+// import Layout from "../Components/Layout";
 import Project from "../Components/Projects";
-import DeleteProject from "../Components/ProjectInfo";
+// import DeleteProject from "../Components/ProjectInfo";
 import useProjects from "../hooks/useProjects"; // Import the custom hook
-import '../Styling/ProjectPageS.scss'
-import { useEffect, useState } from "react";
-// import type { Project as ProjectType } from "../Components/Types";
-import type { HandleProject, Project as ProjectType } from "../Components/Types"
-import useProjectForm from "../hooks/useProjectForm";
+import '../Styling/ProjectPageS.scss';
+import type { HandleProject, Project as ProjectType } from "../Components/Types";
+// import useProjectForm from "../hooks/useProjectForm";
 
-export default function FrontPage() {
-  const { add, remove, update, status, get, data, error } = useProjects();
-  const projects = data;
+
+
+export default function ProjectPage() {
+  const { add, remove, update, status, get, data, error } = useProjects()
+  const projects = data
+  
 
   const handleProjectMutation: HandleProject = (props) => {
-    const { action } = props;
+    const { action } = props
 
     switch (action) {
       case "add":
-        add(props.project);
-        break;
+        add(props.project)
+        break
       case "remove":
-        remove(props.id);
-        break;
+        remove(props.id)
+        break
       case "update":
-        update(props.id, props.project);
-        break;
+        update(props.id, props.project)
+        break
       default:
-        break;
+        break
     }
-  };
+  }
 
-  const addProjectServer = async (id: string) => {
-    try {
-      return fetch("http://localhost:3000", {
-        method: "POST",
-        body: JSON.stringify({
-          note: "",
-          projectId: id,
-        }),
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  // const addProjectServer = async (id: string) => {
+  //   try {
+  //     return fetch("http://localhost:3000", {
+  //       method: "POST",
+  //       body: JSON.stringify({
+  //         note: "",
+  //         projectId: id,
+  //       }),
+  //     })
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
 
   // const addProject = async (id: string) => {
-  //   const updatedStreak = await addProjectServer(id);
+  //   const result = await addProjectServer(id)
+  //   if (!result) return
 
-  //   if (!updatedStreak) return;
+  //   await get()
+  // }
 
-  //   await get();
-  // };
-
-  
-
-  // if (status.loading) return <p>Laster ...</p>;
-  // if (status.error) return <p className="error">{error}</p>;
+  if (status.loading) return <p>Laster ...</p>
+  if (status.error) return <p className="error">{error}</p>
 
   return (
     <>
-       <Project
+      <Project
         projects={projects}
         handleProjectMutation={handleProjectMutation}
+        
       >
       </Project>
     </>
   )
 }
+
